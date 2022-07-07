2c14b2853f4f3ab5f8638efec8d9a71bb6e84334
LukasPil <piliponis.lukas@gmail.com>
2022-07-07 11:58:03 +0300

    Include test logic into TestDemo

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,14 +1,28 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestDemo {
 
     @Test
-    public void testSomething(){
+    public void testSomething() {
+        int variable1 = 5;
+        int variable2 = 3;
+        int actualSum = variable1 + variable2;
+        int expectedSum = 8;
 
+        Assert.assertEquals(actualSum, expectedSum);
     }
 
     @Test
-    public void testSomethingAgain(){
+    public void testSomethingAgain() {
+        String actualText = "Siandien yra pirmadienis";
+//        String expectedText = "antradienis";
+        String expectedText = "pirmadienis";
 
+        Assert.assertTrue(
+                actualText.contains(expectedText)
+//                actualText.compareTo(expectedText) == 0,
+//                String.format("expected: %s, actual: %s ", expectedText, actualText)
+        );
     }
 }
