28a259cbc5844768316a6fe714b6cd51f02d72df
LukasPil <piliponis.lukas@gmail.com>
2022-07-07 11:28:33 +0300

    Add TestDemo into test dir

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestDemo {
+
+    @Test
+    public void testSomething(){
+
+    }
+
+    @Test
+    public void testSomethingAgain(){
+
+    }
+}
